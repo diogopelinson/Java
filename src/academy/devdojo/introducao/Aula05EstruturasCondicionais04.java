5f9cc576d188516c92e763e950ddb8286a3cc44d
Diogo <diogopelinsonduartemoraes@gmail.com>
2025-06-27 01:07:37 -0300

    Terminando exercicio Estruturas condicionais]

--- src/academy/devdojo/introducao/Aula05EstruturasCondicionais04.java
+++ src/academy/devdojo/introducao/Aula05EstruturasCondicionais04.java
@@ -2,19 +2,19 @@ package academy.devdojo.introducao;
 
 public class Aula05EstruturasCondicionais04 {
     public static void main(String[] args) {
-        double salarioAnual = 25000;
+        double salarioAnual = 70000;
         double primeiraFaixa = 9.70 / 100;
         double segundaFaixa = 37.35 / 100;
         double terceiraFaixa = 49.50 / 100;
         double valorImposto;
 
         if (salarioAnual <= 34712) {
-            salarioAnual = salarioAnual * primeiraFaixa;
+            valorImposto = salarioAnual * primeiraFaixa;
         } else if (salarioAnual >= 34713 && salarioAnual <= 68507) {
-            salarioAnual = salarioAnual * segundaFaixa;
+            valorImposto = salarioAnual * segundaFaixa;
         } else {
-            salarioAnual = salarioAnual * terceiraFaixa;
+            valorImposto = salarioAnual * terceiraFaixa;
         }
-        System.out.println(salarioAnual);
+        System.out.println("O valor que sera pago de imposto sera de: "+valorImposto);
     }
 }
