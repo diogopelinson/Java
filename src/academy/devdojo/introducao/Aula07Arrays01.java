5cb7bb2bcfd94110ac0309d83d523fb7f1968152
Diogo <diogopelinsonduartemoraes@gmail.com>
2025-06-30 12:29:37 -0300

    Iniciando Arrays

---
+++ src/academy/devdojo/introducao/Aula07Arrays01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.introducao;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        // pecisa declarar quanto de espaco sera usado em memoria
+        // Arrays sao objetos na memoria
+
+        int[] idades = new int[3];
+        idades[0] = 21;
+        idades[1] = 15;
+        idades[2] = 11;
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
