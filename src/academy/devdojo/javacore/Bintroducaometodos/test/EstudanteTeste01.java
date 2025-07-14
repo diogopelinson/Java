1fdf2a1abede4b72214042ec80a576d5a0f9d7a9
Diogo <diogopelinsonduartemoraes@gmail.com>
2025-07-14 11:53:00 -0300

    Parâmetros tipo referência

---
+++ src/academy/devdojo/javacore/Bintroducaometodos/test/EstudanteTeste01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.javacore.Bintroducaometodos.dominio.Estudante;
+
+public class EstudanteTeste01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+    }
+}
