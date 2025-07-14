61545085bebe8bbb7b98f12e5ccd6e7dc02b6202
Diogo <diogopelinsonduartemoraes@gmail.com>
2025-07-14 12:05:25 -0300

    Parâmetros tipo referência02

--- src/academy/devdojo/javacore/Bintroducaometodos/test/EstudanteTeste01.java
+++ src/academy/devdojo/javacore/Bintroducaometodos/test/EstudanteTeste01.java
@@ -1,9 +1,25 @@
 package academy.devdojo.javacore.Bintroducaometodos.test;
 
 import academy.devdojo.javacore.Bintroducaometodos.dominio.Estudante;
+import academy.devdojo.javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
 
 public class EstudanteTeste01 {
     public static void main(String[] args) {
-        Estudante estudante = new Estudante();
+        Estudante estudante01 = new Estudante();
+        Estudante estudante02 = new Estudante();
+        ImpressoraEstudante impressora = new ImpressoraEstudante();
+
+        estudante01.nome = "Buck";
+        estudante01.idade = 42;
+        estudante01.sexo = 'M';
+
+        estudante02.nome = "Azami";
+        estudante02.idade = 36;
+        estudante02.sexo = 'F';
+
+        impressora.imprime(estudante01);
+        impressora.imprime(estudante02);
+
+
     }
 }
