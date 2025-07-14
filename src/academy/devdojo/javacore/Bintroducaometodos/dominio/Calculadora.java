a4d1c5e60b56c2c120311bf44cfc98820a2cc8e9
Diogo <diogopelinsonduartemoraes@gmail.com>
2025-07-14 11:06:28 -0300

    Retorno em Metodos

--- src/academy/devdojo/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -7,4 +7,13 @@ public class Calculadora {
     public void somaDoisNumeros(){
         System.out.println(10 + 10);
     }
+
+    public void subtraiDoisNumeros(){
+        System.out.println(21 - 2);
+    }
+
+    // Parâmetros: Variaveis locais, vivas durante o metodo
+    public void multiplicaDoisNumeros(int num1, float num2){
+        System.out.println(num1 * num2);
+    }
 }
--- src/academy/devdojo/javacore/Bintroducaometodos/test/CalculadoraTeste02.java
+++ src/academy/devdojo/javacore/Bintroducaometodos/test/CalculadoraTeste02.java
@@ -2,10 +2,11 @@ package academy.devdojo.javacore.Bintroducaometodos.test;
 
 import academy.devdojo.javacore.Bintroducaometodos.dominio.Calculadora;
 
-public class CalculadoraTeste01 {
+public class CalculadoraTeste02 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        calculadora.somaDoisNumeros();
-        calculadora.subtraiDoisNumeros();
+
+        // Usando os Parâmetros como Argumentos(os numeros):
+        calculadora.multiplicaDoisNumeros(19,15.5F);
     }
 }
